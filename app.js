833aec9352f666623bee7b7a8359691f2ec21e43
Nepcam <cameron.nepe@gmail.com>
2020-01-12 21:44:11 +1300

    working on placing items on the UI

--- app.js
+++ app.js
@@ -86,6 +86,21 @@ const UICtrl = (function(){
         calories:document.querySelector(UISelectors.itemCaloriesInput).value
       }
     },
+    addListItem: function(item){
+      // Create li element
+      const li = document.createElement('li');
+      // Add class
+      li.className = 'collection-item';
+      // Add ID
+      li.id = `item-${item.id}`;
+      // Add html
+      localStorage.innerHTML = `<strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+      <a href="#" class="secondary-content">
+        <i class="edit-item fa fa-pencil"></i>
+      </a>`;
+      // Insert item
+      document.querySelector(UISelectors.itemList).insertAdjacentElement('beforeend', li);
+    },
     getSelectors: function(){
       return UISelectors;
     }
@@ -114,6 +129,8 @@ const App = (function(ItemCtrl, UICtrl){
     if(input.name !== '' && input.calories !== ''){
       // Add item
       const newItem = ItemCtrl.addItem(input.name, input.calories);
+      // Add item to UI list
+      UICtrl.addListItem(newItem);
     }
 
     e.preventDefault();
